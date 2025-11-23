6f1b906d0e3b880d73a3d3d0c82ce20cd276dc94
Vansh Thapar <vanshthapar.professional@gmail.com>
2025-11-23 14:11:39 +0530

    fixed the tiDB ssl issue

--- server/db/db.js
+++ server/db/db.js
@@ -8,9 +8,14 @@ const pool = mysql.createPool({
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
+    port: process.env.DB_PORT || 4000,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    sl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    }
 })
 
 pool.query('SELECT 1+1').then(() => {
